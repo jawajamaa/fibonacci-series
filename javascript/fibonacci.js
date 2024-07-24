ef4c2eeb0b5da9284a6b1cedbad7df648fb2df88
Timothy Ryon <tim@timryon.com>
2024-07-24 16:27:48 -0400

    finish js code and pass all tests

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,31 @@
 function fibonacci(num) {
-  // type your code here
+
+  if (num === 0) {
+    return 0
+
+  } else if (num === 1 || num === 2) {
+    return 1
+
+  } else {
+    const a = 0;
+    const b = 1;
+    const c = 1;
+    const fib = [a, b, c]
+    for (let n = 2; n <= num; n++) {
+        d = fib[fib.length - 2] + fib[fib.length - 1]
+        // console.log("Line 18, d:", d)
+        fib.push(d)
+        // console.log("Line 20, fib:", fib)
+      } 
+    const ans = fib[`${num}`]
+    // console.log("Line 22, fib:", fib)
+    // console.log("Line 23, num-1:", `${num-1}`)
+    // console.log("Line 24, ans:", ans)
+    return ans
+  }
 }
+
+// console.log(fibonacci(10))
 
 if (require.main === module) {
   // add your own tests in here
@@ -20,5 +45,6 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// parameter 'num' is the nth digit of the sequence
+// sequence begins with 0,1 and continues based on value of 'num' given as arg
+// declare two variables for the succeeding two integers?
